4d8064c483472bebc8d342de07b361cfd81c4336
Eric <ericcui@qq.com>
2023-12-29 18:46:11 +0800

    add sample code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Collections = new List<object>
+            {
+                new object(),
+            };
+            BindingContext = this;
+
+        }
+
+        public List<object> Collections { get; set; }
+    }
+
+}
